1a3762c0a1ef171c2f34b0c179368d115afd98fd
chuongvu2001 <chuongvu2806@gmail.com>
2021-03-17 14:38:50 +0700

    update router

--- routes/product.js
+++ routes/product.js
@@ -5,10 +5,5 @@ router.get('/products',(req,res) =>{
     res.json({
         mess: 'success'
     })
-})
-router.post('/products',(req,res) =>{
-    res.json({
-        mess: 'success'
-    })
-})
+});
 module.exports = router;
